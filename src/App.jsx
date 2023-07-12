d0bac514dda90f1f1cdb5e58f979e71c36f42fb5
aanmeba <aanmeba@gmail.com>
2023-07-12 21:23:19 +1000

    implement firestore

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 // import "./App.css";
 import Home from "./pages/Home/Home";
+import { getAllProducts } from "./services/firestore-services";
 
 function App() {
+  getAllProducts();
   return (
     <>
       <Home />
