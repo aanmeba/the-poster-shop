be3230324fbb8a27adbaea94b6771282e1c21ee9
aanmeba <aanmeba@gmail.com>
2023-07-15 22:01:39 +1000

    add icons component

--- src/components/FontAwesomeIcons/FontAwesomeIcons.jsx
+++ src/components/FontAwesomeIcons/FontAwesomeIcons.jsx
@@ -1,5 +1,7 @@
 import {
   faCartShopping,
+  faChevronLeft,
+  faChevronRight,
   faHeart as faHeartSolid,
 } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as faHeartEmpty } from "@fortawesome/free-regular-svg-icons";
@@ -8,10 +10,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const HeartSolid = () => {
   return <FontAwesomeIcon icon={faHeartSolid} />;
 };
+
 export const HeartEmpty = () => {
   return <FontAwesomeIcon icon={faHeartEmpty} />;
 };
 
 export const ShoppingCart = () => {
   return <FontAwesomeIcon icon={faCartShopping} />;
+};
+
+export const ArrowRight = () => {
+  return <FontAwesomeIcon icon={faChevronRight} />;
+};
+
+export const ArrowLeft = () => {
+  return <FontAwesomeIcon icon={faChevronLeft} />;
 };
