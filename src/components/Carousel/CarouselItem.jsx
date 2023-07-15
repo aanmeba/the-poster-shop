8410b8bcefc6b8f64bed1ba2c1c5c43c7e14720d
aanmeba <aanmeba@gmail.com>
2023-07-15 22:02:51 +1000

    mostly completed carousel components

--- src/components/Carousel/CarouselItem.jsx
+++ src/components/Carousel/CarouselItem.jsx
@@ -1,10 +1,34 @@
+import { HeartEmpty } from "../FontAwesomeIcons/FontAwesomeIcons";
 import styles from "./Carousel.module.scss";
 
 const CarouselItem = ({ item }) => {
+  const onClick = (e) => {
+    // add favourite list
+    // 🚨 how to get the unique id from the item
+    console.log(e.currentTarget.id);
+  };
+
   return (
     <div className={styles.slide__item}>
-      <h3>{item.title}</h3>
-      <p>{item.price}</p>
+      <figure>
+        <img src="../../../public/poster_4.jpeg" alt="" />
+      </figure>
+      <div className={styles.slide__item__info}>
+        <div className={styles.slide__item__info__top}>
+          <h5 className={styles.slide__item__info__top__title}>{item.title}</h5>
+          <div
+            onClick={onClick}
+            id="favIcon"
+            className={styles.slide__item__info__top__icon_wrapper}
+          >
+            <HeartEmpty className={styles.heart} />
+          </div>
+        </div>
+
+        <div className={styles.slide__item__info__bottom}>
+          <p>{item.price}</p>
+        </div>
+      </div>
     </div>
   );
 };
