8a7521a6c21b27ae5902420bb88295b3f2e37960
aanmeba <aanmeba@gmail.com>
2023-07-17 21:27:38 +1000

    add collection card components

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,11 @@
 // import { NavLink } from "react-router-dom";
-import {
-  HeartEmpty,
-  HeartSolid,
-  ShoppingCart,
-} from "../FontAwesomeIcons/FontAwesomeIcons";
+import { HeartSolid, ShoppingCart } from "../FontAwesomeIcons/FontAwesomeIcons";
 import styles from "./NavBar.module.scss";
 
 const NavBar = () => {
   return (
     <nav className={styles.nav}>
       <span>Products</span>
-      {/* <HeartEmpty /> */}
       <HeartSolid />
       <ShoppingCart />
     </nav>
