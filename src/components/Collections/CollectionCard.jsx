b737723d26b2d82f8b5f62923bf6cd33879b326c
aanmeba <aanmeba@gmail.com>
2023-07-17 21:27:38 +1000

    add collection card components

--- src/components/Collections/CollectionCard.jsx
+++ src/components/Collections/CollectionCard.jsx
@@ -2,12 +2,21 @@
 
 import styles from "./Collection.module.scss";
 
-const CollectionCard = () => {
+const CollectionCard = ({ content }) => {
+  const { title, imageUrl } = content;
+  const bgImage = `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${imageUrl})`;
   return (
     <div className={styles.card}>
-      <h3 className={styles.card__title}>Collection Name</h3>
-      {/* <Link */}
-      <button className={styles.card__link}>Shop more</button>
+      <div
+        className={styles.card__img}
+        style={{ backgroundImage: bgImage }}
+      ></div>
+      <div className={styles.card__text}>
+        <h3 className={styles.card__text__title}>{title}</h3>
+        <span className={styles.card__text__linkWrapper}>
+          <span className={styles.card__text__link}>Explore</span>
+        </span>
+      </div>
     </div>
   );
 };
--- src/components/Collections/CollectionWrapper.jsx
+++ src/components/Collections/CollectionWrapper.jsx
@@ -2,10 +2,23 @@ import styles from "./Collection.module.scss";
 import CollectionCard from "./CollectionCard";
 
 const CollectionWrapper = () => {
+  const collectionData = [
+    {
+      title: "Most Popular",
+      imageUrl:
+        "https://images.unsplash.com/photo-1631509716275-59e9d106504a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1486&q=80",
+    },
+    {
+      title: "New Arrivals",
+      imageUrl:
+        "https://images.unsplash.com/photo-1632566853092-a0faa4665585?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=899&q=80",
+    },
+  ];
   return (
     <section className={styles.container}>
-      <CollectionCard />
-      <CollectionCard />
+      {collectionData.map((c, i) => (
+        <CollectionCard key={i} content={c} />
+      ))}
     </section>
   );
 };
