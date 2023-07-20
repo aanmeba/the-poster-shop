58f7cc015a1006e53b0b49f1350bd266bc46d7cf
aanmeba <aanmeba@gmail.com>
2023-07-20 22:56:32 +1000

    modify favourite icons state based on the favourite item list

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,36 +13,45 @@ const NavBar = ({ dark }) => {
   const styleList = dark ? styles.nav__text__dark : styles.nav__text__light;
 
   const { globalState } = useContext(GlobalContext);
-  const defaultHasItems = { favourites: false, cart: false };
-  const [hasItems, setHasItems] = useState(defaultHasItems);
-  // const [hasFav, setHasFav] = useState(false);
-  // const [hasCart, setHasCart] = useState(false);
+  // const defaultHasItems = { favourites: false, cart: false };
+
+  /***
+   * Can't I control the true/false using object?? 🚨
+   *
+   */
+  // const [hasItems, setHasItems] = useState(defaultHasItems);
+  const [hasFav, setHasFav] = useState(false);
+  const [hasCart, setHasCart] = useState(false);
   const { favItems, cartItems } = globalState;
 
   useEffect(() => {
     if (favItems.length > 0) {
       console.log(favItems.length, "--- favItems length");
-      setHasItems({ ...hasItems, favourites: true });
-      // setHasFav(true);
+      // setHasItems({ ...hasItems, favourites: true });
+      setHasFav(true);
+    } else {
+      setHasFav(false);
     }
     if (cartItems.length > 0) {
       console.log(cartItems.length, "--- cartItems length");
-      setHasItems({ ...hasItems, cart: true });
-      // setHasCart(true);
+      // setHasItems({ ...hasItems, cart: true });
+      setHasCart(true);
+    } else {
+      setHasCart(false);
     }
   }, [favItems, cartItems]);
 
   useEffect(() => {
-    console.log(hasItems, globalState, "--- navbar");
-  }, [hasItems, globalState]);
+    console.log(globalState, hasFav, hasCart, "--- navbar");
+  }, [globalState]);
 
   return (
     <nav className={styles.nav}>
       <NavLink to="/products" style={{ textDecoration: "none" }}>
         <span className={styleList}>Products</span>
       </NavLink>
-      {/* {hasFav ? <HeartSolid /> : <HeartEmpty />} */}
-      {hasItems.favourites ? <HeartSolid /> : <HeartEmpty />}
+      {hasFav ? <HeartSolid /> : <HeartEmpty />}
+      {/* {hasItems.favourites ? <HeartSolid /> : <HeartEmpty />} */}
       {/* <ShoppingCart /> */}
       <ShoppingBag />
     </nav>
