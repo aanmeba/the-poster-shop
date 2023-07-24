f091120e20750f88b6301c72a520ee5bab68e679
aanmeba <aanmeba@gmail.com>
2023-07-24 17:10:38 +1000

    resolve build error

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import Carousel from "../../components/Carousel/Carousel";
 import CollectionWrapper from "../../components/Collections/CollectionWrapper";
 import Hero from "../../components/Hero/Hero";
-import Subtitle from "../../components/SubTitle/SubTItle";
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <Subtitle>New Arrivals</Subtitle>
       <Carousel />
       <CollectionWrapper />
     </>
